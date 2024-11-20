bc14272655ac2c7d0e162e8046602495ba45b4a2
umais <professional.umais@gmail.com>
2024-11-20 14:43:51 +0500

    faq

--- index.js
+++ index.js
@@ -34,3 +34,18 @@ const swiper = new Swiper(".slider-wrapper", {
     },
   },
 });
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
+}
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
